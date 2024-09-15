3c97852bd7da43ef9e7609d66cbcf9e4430e031c
Sebastian Dąbrowski <sdabrowski@listonic.com>
2024-09-15 13:50:04 +0200

    Initial Commit

---
+++ ThreadsApp/ThreadsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsAppApp.swift
+//  ThreadsApp
+//
+//  Created by Sebastian Dąbrowski on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
